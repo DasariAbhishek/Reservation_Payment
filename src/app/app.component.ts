4bbed55f97f2b6b4f67aab78460248c8e33a8826
Abhishek Dasari <abhishekdasari111@gmail.com>
2022-10-07 15:04:58 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { ReservationDetail } from './shared/reservation-detail.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DemoApp';
+  // title = 'DemoApp';
+  // name: string = "Angular Application";
+  // isDisabled = false;
+  // textId = "sampleId";
+
+  constructor(private fb: FormBuilder){}
+
+  // ReservationForm = new FormGroup({
+  //   name : new FormControl(''),
+  //   age : new FormControl(),
+  //   phone : new FormControl(''),
+  // })
+
+  // isSubmitted = false
+  // quotas = ['General','Ladies'];
+
+  // get quotaName() {
+  //   return this.ReservationForm.get('quotaName');
+  // }
+
+  // ReservationForm = this.fb.group({
+  //   quotaName: ['', [Validators.required]],
+  // });
+
+
+  // changeQuota(e: any) {
+  //   this.quotaName?.setValue(e.target.value, {
+  //     onlySelf: true,
+  //   });
+  // }
+
+  // onSubmit(): void {
+  //   console.log(this.ReservationForm);
+  //   this.isSubmitted = true;
+  //   if (!this.ReservationForm.valid) {
+  //     false;
+  //   } else {
+  //     console.log(JSON.stringify(this.ReservationForm.value));
+  //   }
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent} from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ReservationService } from './shared/reservation.service';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { AppRoutingModule } from './app-routing.module';
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: 'PaymentDetails',component: PaymentDetailsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PaymentDetailsComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [RouterModule],
+  providers: [ReservationService],
+  bootstrap: [AppComponent,HomeComponent]
 })
 export class AppModule { }
